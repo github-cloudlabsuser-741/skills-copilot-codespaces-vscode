f1f4db126b0122df709b4c874700c553dde7904d
github-cloudlabsuser-741 <167431281+github-cloudlabsuser-741@users.noreply.github.com>
2024-05-06 00:43:43 +0000

    Copilot second commit

---
+++ prime.py
@@ -0,0 +1,10 @@
+def prime(n):
+    """
+    Returns True if n is a prime number, False otherwise.
+    """
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
